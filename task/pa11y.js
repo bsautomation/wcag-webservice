116e40c30822cb011d52705771672454956d6181
Kush Shah <kush@browserstack.com>
2021-05-03 10:13:22 +0530

    change in cron

--- task/pa11y.js
+++ task/pa11y.js
@@ -1,17 +1,17 @@
-// This file is part of Pa11y Webservice.
+// This file is part of AxeTest Webservice.
 //
-// Pa11y Webservice is free software: you can redistribute it and/or modify
+// AxeTest Webservice is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Pa11y Webservice is distributed in the hope that it will be useful,
+// AxeTest Webservice is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Pa11y Webservice.  If not, see <http://www.gnu.org/licenses/>.
+// along with AxeTest Webservice.  If not, see <http://www.gnu.org/licenses/>.
 'use strict';
 
 const async = require('async');
@@ -19,7 +19,7 @@ const {green, grey, red} = require('kleur');
 const {CronJob} = require('cron');
 
 module.exports = initTask;
-exports.runPa11yOnTasks = runPa11yOnTasks;
+exports.runAxeTestOnTasks = runAxeTestOnTasks;
 
 // Initialise the task
 function initTask(config, app) {
@@ -36,8 +36,8 @@ async function taskRunner(app) {
 	console.log(grey('Starting to run task @ %s'), new Date());
 
 	try {
-		const tasks = await app.model.task.getAll();
-		runPa11yOnTasks(tasks, app);
+		const tasks = await app.model.axeTask.getAll();
+		runAxeTestOnTasks(tasks, app);
 	} catch (error) {
 		console.error(red('Failed to run task: %s'), error.message);
 		console.log('');
@@ -45,20 +45,20 @@ async function taskRunner(app) {
 	}
 }
 
-// Runs an array of pa11y tasks
-function runPa11yOnTasks(tasks, app) {
+// Runs an array of AxeTest tasks
+function runAxeTestOnTasks(tasks, app) {
 	if (tasks.length === 0) {
-		console.log('No pa11y tasks to run');
+		console.log('No AxeTest tasks to run');
 		return;
 	}
 
 	const worker = async task => {
-		console.log('Starting pa11y task %s', task.id);
+		console.log('Starting AxeTest task %s', task.id);
 		try {
-			await app.model.task.runById(task.id);
-			console.log(green('Finished pa11y task %s'), task.id);
+			await app.model.axeTask.runAxeById(task.id);
+			console.log(green('Finished AxeTest task %s'), task.id);
 		} catch (error) {
-			console.log(red('Failed to finish pa11y task %s: %s'), task.id, error.message);
+			console.log(red('Failed to finish AxeTest task %s: %s'), task.id, error.message);
 		}
 	};
 
@@ -66,6 +66,6 @@ function runPa11yOnTasks(tasks, app) {
 	queue.push(tasks);
 
 	queue.drain(() => {
-		console.log(grey('Finished running pa11y tasks @ %s'), new Date());
+		console.log(grey('Finished running AxeTest tasks @ %s'), new Date());
 	});
 }
